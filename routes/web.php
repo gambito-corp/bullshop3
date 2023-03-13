4a092dc04819e995215dbb75195e1066c03e19fe
Gambito Code/ <asesor.pedro@gmail.com>
2023-03-13 17:02:56 +0100

    ajustes2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\CustomAuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return redirect()->route('login');
 });
+
+Route::get('login', [CustomAuthController::class, 'showLoginForm'])->name('login');
 
 Route::middleware([
     'auth:sanctum',
